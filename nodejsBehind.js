dbfdcbe0984c5f6077dc71374a2732890bc365f2
hamzaali <hamza54834@gmail.com>
2020-02-12 10:46:05 -0800

    Node.js Concept of SetTimeout

--- nodejsBehind.js
+++ nodejsBehind.js
@@ -1,12 +1,17 @@
 ///////////////////////Event Loop in practice  
 const fs=require('fs');
-setTimeout(()=>{console.log("Hello world "),10})  //First Timer
-setTimeout(()=>{console.log("Hello World 2"),20}) //Second Timer
+// setTimeout(()=>{console.log("Hello world "),10})  //First Timer
+// setTimeout(()=>{console.log("Hello World 2"),20}) //Second Timer 
 
 console.log("Hello 3")  //Top Level code
 
-fs.readFile('.\NodeBehind\test-file.txt',()=>{   //
+fs.readFile('.\NodeBehind\test-file.txt',()=>{   //last Code
     console.log("I/O finished");
+
+
+    setTimeout(()=>console.log("Hello world "),10)  //First Timer
+setTimeout(()=>console.log("Hello World 2"),3000)
+setTimeout(()=>console.log("Hello world Test value 0"),0) //Second Timer 
 })
 
 console.log("Hello 4");   // Top level Code
