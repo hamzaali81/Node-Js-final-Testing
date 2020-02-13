caf4c230a89517ea216813c346955ffd2d145423
hamzaali <hamza54834@gmail.com>
2020-02-13 10:57:53 -0800

    Events, streams

--- NodeBehind/events/streamsSolution2.js
+++ NodeBehind/events/streamsSolution2.js
@@ -3,14 +3,27 @@ const fs=require('fs');
 const server =require('http').createServer();  //http object
 
 server.on('request',(req,res)=>{
-const readable=fs.createReadStream('test-file.txt');
+const readable=fs.createReadStream('testt-file.txt');  //testtt ===error file
 readable.on('data',chunk=>{
     res.write(chunk);  //write method every single piece of data into that stream
 })
-res.end
-
+readable.on('end',()=>{
+    res.end();  //response is also a stream and end method signals that no more data will be written 
+})
+readable.on('error',err=>{
+    console.log(err);
+    res.statusCode=500;                  //res.status(500) //This is express  //500 server error
+                     //404 page not found error
+    res.end('file not found!')
+})
 });
 
 server.listen(3000,'127.0.0.1',()=>{
     console.log("Listeining..........")
 })
+
+//This Approach is problem is readable stream so the one that we are using to read
+//the file from the disk is much faster than actually sending the the result with response writeable stream over the network.And this is overwhelm the response
+//stream which cannot handle all this incoming data so fast and this problem is so called backpressure.
+//and its real problem in real situation so in this case backpressurehappens when the response 
+//cannot send data nearly as fast as it receiving it from the file
---
+++ NodeBehind/events/streamsSolution3.js
@@ -0,0 +1,20 @@
+const fs=require('fs');
+const server =require('http').createServer();  //http object
+
+//Pipe operator is available for all readable streams and its allows us to pipe the output of a readable stream
+//Right into input to writeable stream 
+//Basically fix the problem of backpressure about
+//handle the speed basically of the data coming in and the speed data going out 
+
+
+//This stream is here duplex
+//pipe method fix backpressure problem automatically solve
+server.on('request',(req,res)=>{
+const readable=fs.createReadStream("test-file.txt")
+//readableSource.pipe(writeDestination)
+readable.pipe(res)
+})
+
+server.listen(3000,'127.0.0.1',()=>{
+    console.log("Listeining..........")
+})
