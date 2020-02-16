7b0a77243a56d5ac2a9b926402f73201d6324178
hamzaali <hamza54834@gmail.com>
2020-02-16 06:49:06 -0800

    middleware implement

--- expressImplement/app.js
+++ expressImplement/app.js
@@ -1,20 +1,9 @@
 const express=require('express')
 const fs = require('fs')
 const app=express();
-//Parse  string data convert in object
-//file base api
 
-///////////////////////////////////////////////////////////////////////
-//Middleware
-app.use(express.json());  //express.json is a middleware
-//middleware is basically a function that can modify the incoming request data
-//called middleware (middle b/w request and response)
+app.use(express.json());  
 
-
-
-//We don't inside the route handler
-//Because the top level code which un execute after
-//Only callback function inside the loop. We cannot have any blocking code but outside their is no problem
 const tours=JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
     );
@@ -24,19 +13,25 @@ const tours=JSON.parse(
         status:'sucess',
         result: tours.length,
         data: { 
-            tours //ES6 feature key and value noy same
-        }                     //data propperty
+            tours
+        }                 
     })
-//Route handler
 });
 
 app.post('/api/v1/tours',(req,res)=>{
-     //Send data to the client ideally available on the request
-     //Express doesnot put body data on the request and in order data is avilable we have to use 
-     //use some middleware
-
-    console.log(req.body)  //body is the property that is avilable to be req.body because we use that middleware
-     res.send('Done') //Some place holder
+    // console.log(req.body) 
+    const newID=tours[tours.length-1].id;
+    const newTour=Object.assign({id:newID},req.body)
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),err=>{
+        res.status(201).json({
+            status:'sucess',
+            data:{ 
+                tour:newTour
+            }
+        })
+    })
+     res.send('Done') 
 })
 
 const port=3000;
