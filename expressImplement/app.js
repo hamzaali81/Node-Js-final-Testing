84b5cdf5da5b612909dd1b521e14343d202e5389
hamzaali <hamza54834@gmail.com>
2020-02-19 09:33:05 -0800

    Code modify

--- expressImplement/app.js
+++ expressImplement/app.js
@@ -83,17 +83,21 @@ res.status(204).json({  //Means no content
 }
 
 // app.get('/api/v1/tours', getAllTours); //Get function directly
+//app.post('/api/v1/tours',createTour)
 app.get('/api/v1/tours/:id',getTour);
-app.post('/api/v1/tours',createTour)
 app.patch('/api/v1/tours/:id',updateTour)
 app.delete('/api/v1/tours/:id',deleteTour)
 ////////////////////////////////////////////////////////////////////////////
 //code more modify
-
-
-app.route('/api/v1/tours')
+//The two route handlers reuse
+app
+.route('/api/v1/tours')
 .get(getAllTours)
 .post(createTour)
+
+app
+.route('/api/v1/tours/:id')
+.get(getTour)
 .patch(updateTour)
 .delete(deleteTour)
 
